cd150c8bbcc5d6daf7f819d06d6e9e775796594d
webdev480 <webdev480@outlook.com>
2022-03-01 22:51:44 -0800

    Nucampsite-Bootstrap

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+function projectNucampsite() {
+    window.open("https://github.com/webdev480/Nucampsite-Bootstrap", "_blank");
+}
+
 function project() {
     window.open("https://github.com/webdev480", "_blank");
 }
